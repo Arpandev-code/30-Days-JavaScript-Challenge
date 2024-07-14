34ae37fc978b82e9c94588f07313f838e45669f5
Arpandev-code <arpan.dev2016@gmail.com>
2024-07-14 21:35:08 +0530

    Day1-Variables

---
+++ 1_Variables/index.js
@@ -0,0 +1,42 @@
+//Activities
+//1.Variable Declaration
+//Task1: Declare a variables using var, assign it a number, and log the value to the console
+var a = 10;
+console.log(a);
+//Task2: Declare a variables using let, assign it a string and log the value to the console
+let name="Arpandev"
+console.log(name);
+//2. Constant Declaration
+//Task3: Declare a variables using const, assign it a boolean and log the value to the console
+const hasName=true;
+console.log(hasName);
+//3. Data Types
+//Task4: Create variables of Different data types(number,string,boolean,object,array) and log each variables's type using the typeof operator
+let money= 200;
+let course= "Welcome to JS 30 days Program"
+let isPaid= true
+let courseDetails={
+    name:"JS Course",
+    price:"Free"
+}
+const videos=["video1","video2","video3"]
+const date=new Date("2024-06-30")
+
+console.log(typeof money);
+console.log(typeof course);
+console.log(typeof isPaid);
+console.log(typeof courseDetails);
+console.log(typeof videos);
+console.log(typeof date);
+
+//4. Reassigning Variables
+//Task5;
+let initial=0;
+console.log(initial);
+initial=30;
+console.log(initial);
+//5. Unserstanding const
+//Task6:
+const init=0;
+init =30;
+
